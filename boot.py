df5896132d3ba0b19a0668ba93bfae3732d6ed1b
Dmitry Sereda <70698691+radi0cat@users.noreply.github.com>
2022-11-29 07:46:29 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -7,7 +7,7 @@ def do_connect():
     wlan.active(True)
     if not wlan.isconnected():
         print('connecting to network...')
-        wlan.config(essid='esp', password='51-eSoram', max_clients=3, authmode=network.AUTH_WPA_WPA2_PSK)
+        wlan.config(essid='esp32', password='12345678', max_clients=3, authmode=network.AUTH_WPA_WPA2_PSK)
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
